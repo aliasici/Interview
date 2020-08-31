9232220c558c0e5a675d7db2ed06d07a8dac3c6e
aliasici <asiciali@yahoo.com>
2020-08-30 20:19:14 -0700

    Uniques2

---
+++ src/JavaInterview/Uniques2.java
@@ -0,0 +1,29 @@
+package JavaInterview;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Uniques2 {
+    public static void main(String[] args) {
+        String str = "AAABBCBBDDEE";
+        String result = "";
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(String each : str.split("")){
+            list.add(each);
+        }
+        System.out.println(list);
+
+        for(String each : list){
+            int count = Collections.frequency(list,each);
+            if(count ==1){
+                result += each;
+            }
+        }
+        System.out.println(result);
+
+
+
+    }
+}
