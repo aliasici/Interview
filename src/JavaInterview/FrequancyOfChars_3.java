cc6773ef834cb158376ecb974deb1868c97e906b
aliasici <asiciali@yahoo.com>
2020-10-24 11:12:10 -0700

    first commit

---
+++ src/JavaInterview/FrequancyOfChars_3.java
@@ -0,0 +1,26 @@
+package JavaInterview;
+
+import Library.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FrequancyOfChars_3 {
+    public static void main(String[] args) {
+
+        String str = "AAABBC";
+
+        ArrayList<String > list = new ArrayList<>();
+        list.addAll(Arrays.asList(str.split("")));
+
+        System.out.println(list);
+        String nonDup=  Util.removeDup(str); //"ABC"   to avoid duplication
+
+        for(String  each : nonDup.split("")){ // each: A, B, C
+            int count = Collections.frequency(list,each);
+            System.out.print(each+count);
+        }
+        }
+
+}
