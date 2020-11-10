a2a437cfe93dd22d8bf820399c56817510879d90
aliasici <asiciali@yahoo.com>
2020-11-10 13:45:56 -0800

    Find Largest Kth

---
+++ src/LeetCode/Arrays/FindKthLargest.java
@@ -0,0 +1,24 @@
+package LeetCode.Arrays;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class FindKthLargest {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,88,77,66};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(findKthLargest(arr, 4));
+
+    }
+    public static int findKthLargest(int[] nums, int k){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for(int i : nums){
+            minHeap.add(i);
+            if(minHeap.size() > k){
+                minHeap.remove();
+            }
+        }
+        return minHeap.remove();
+    }
+}
